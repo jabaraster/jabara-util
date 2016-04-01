153b5911f356bd49bbdcd45dcfc4f64ec7d6ac93
jabaraster <jabaraster@gmail.com>
2016-04-01 16:41:34 +0900

    mainte.

--- src/Jabara/Util/Network.hs
+++ src/Jabara/Util/Network.hs
@@ -1,11 +1,15 @@
 module Jabara.Util.Network (
   parsePortNumber
+  , localHostAddress
 ) where
 
 import Data.Either
 import GHC.Base
 import GHC.Num
-import Network.Socket (PortNumber(..))
+import Network.Socket (withSocketsDo, getAddrInfo
+                      , socket, Family(AF_INET), SocketType(Datagram), defaultProtocol
+                      , connect, addrAddress, getSocketName, close
+                      , PortNumber(..))
 import Text.Read (readMaybe)
 
 parsePortNumber :: String -> Either String PortNumber
@@ -13,3 +17,12 @@ parsePortNumber s = case readMaybe s of
                       Nothing -> Left ("invalid port number -> " ++ s)
                       Just i  -> Right $ fromInteger i
 
+localHostAddress :: IO String
+localHostAddress = withSocketsDo $ do
+    (serveraddr:_) <- getAddrInfo Nothing (Just "128.0.0.0") (Just "echo")
+    s <- socket AF_INET Datagram defaultProtocol
+    connect s (addrAddress serveraddr)
+    sockAddr <- getSocketName s
+    let adrs = takeWhile (/=':') $ show sockAddr
+    close s
+    return adrs
